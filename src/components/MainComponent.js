3005f4078c59e0997ed12bf0b92a3c2a2ba007ee
Wendel <wendel.simoes.br@gmail.com>
2021-02-03 09:20:36 -0300

    Just doing some tests

---
+++ src/components/MainComponent.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './MenuComponent';
+import DishDetail from './DishdetailComponent';
+import { DISHES } from '../shared/dishes';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+class Main extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+      selectedDish: null
+    };
+  }
+
+  onDishSelect(dishId) {
+    this.setState({ selectedDish: dishId });
+  }
+
+  render() {
+    const HomePage = () => {
+      return (
+        <Home
+        />
+      );
+    }
+
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Header />
+        <Switch>
+          <Route path='/home' component={HomePage} />
+          <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} onClick={() => this.onDishSelect(dishId)} />} />
+          <Redirect to="/home" />
+        </Switch>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Main;
